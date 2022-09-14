31bda36d0267260ef03824d71381d5163346707e
Sergio <sergiomendes23@gmail.com>
2022-09-13 22:31:44 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+let db;
+
+mongoClient.connect().then(() => {
+    db = mongoClient.db('drivenmugs');
+});
+
+
+server.listen(5000, () => {console.log('Servidor rodando na porta 5000')}) 
