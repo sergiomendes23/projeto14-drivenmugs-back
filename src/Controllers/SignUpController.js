d163e3819625b9cf8413f3a92278a96bc7f1aeff
C <jotapedeside@gmail.com>
2022-09-16 14:06:24 -0300

    refactor(SignUp Controller): fixed exporting

--- src/Controllers/SignUpController.js
+++ src/Controllers/SignUpController.js
@@ -1,19 +1,17 @@
 import bcrypt from 'bcrypt';
 import { db } from '../mongo.js';
 
-dotenv.config();
-
-export async function signUp(req, res){
+const signUp = async (req, res) => {
   const {name, email, password} = req.body;
     try {
       const hash = bcrypt.hashSync(password, 10);
-      const validEmail = await db.collection('Users').findOne({email});
+      const validEmail = await db.collection('users').findOne({email});
 
       if (validEmail) {
         return res.status(409).json({ status: 409, message: "Email já utilizado" });
       }
       
-      await db.collection('Users').insertOne({name, email, password: hash})
+      await db.collection('users').insertOne({name, email, password: hash})
       
       return  res.status(201).json({ status: 201, message: "Usuário criado com sucesso" });
 
@@ -22,3 +20,5 @@ export async function signUp(req, res){
       res.status(500);
     }
 }
+
+export default signUp;
