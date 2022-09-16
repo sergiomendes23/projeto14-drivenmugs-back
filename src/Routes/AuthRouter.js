b9f883be7b80d408545a35719b303686d54725d9
C <jotapedeside@gmail.com>
2022-09-16 14:50:54 -0300

    refactor(AuthRouter): updated to receive existing middleware and controller SignIn procedures

--- src/Routes/AuthRouter.js
+++ src/Routes/AuthRouter.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import signUp from '../Controllers/SignUpController.js';
 import validateSignUp from '../Middlewares/SignUpMiddleware.js';
-import { signIn } from '../Controllers/AuthControler.js';
+import signIn from '../Controllers/SignInController.js';
+import validateSignIn from '../Middlewares/SignInMiddleware.js';
 
 const router = Router();
-
 router.post('/cadastro', validateSignUp, signUp);
-router.post('/login', signIn);
+router.post('/login', validateSignIn, signIn);
 
 export default router;
