d6c24fa9f2e080c5ae96b2e9bf3c0e339405767e
C <jotapedeside@gmail.com>
2022-09-18 18:23:51 -0300

    refactor(MugsController): passing mugs in a simpler way

--- src/Controllers/MugsController.js
+++ src/Controllers/MugsController.js
@@ -3,7 +3,7 @@ import { db } from '../mongo.js';
 const listMugs = async (req, res) => {
   try {
     const mugs = await db.collection('mugs').find().toArray();
-    res.status(200).json({ status: 200, message: mugs });
+    res.status(200).send(mugs);
   } catch (error) {
     res.status(500);
   }
@@ -13,7 +13,7 @@ const retrieveMug = async (req, res) => {
   const { id } = req.params;
   try {
     const mug = await db.collection('mugs').find({id: Number(id)}).toArray();
-    res.status(200).json({ status: 200, message: mug });
+    res.status(200).send(mug);
   } catch (error) {
     res.status(500);
   }
